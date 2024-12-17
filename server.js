a6b0ef1cefe9b7e3d1a283d0a98f7cbcf56f95f8
cihatturgut <cihatturgut@yaani.com>
2024-12-17 04:31:37 +0300

    Express-handlebars template engine & prettier plugin installed

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express=require('express');
-const path = require('path');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));    
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
 });
-app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));    
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
 });
-app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));    
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
 });
-app.get('/blog', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/blog.html'));    
+app.get("/blog", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/blog.html"));
 });
-
-
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on http://${process.env.HOST}:${process.env.PORT}`);
+  console.log(
+    `Server running on http://${process.env.HOST}:${process.env.PORT}`
+  );
 });
