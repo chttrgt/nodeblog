93cbe7a383618eac983fdb5354a83c2c87a0f720
cihatturgut <cihatturgut@yaani.com>
2024-12-18 03:35:43 +0300

    Post detail page created

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -7,6 +7,17 @@ router.get("/new", (req, res) => {
   res.render("pages/addNewPost");
 });
 
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .lean()
+    .then((post) => {
+      res.render("pages/postDetails", { post });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 router.post("/test", (req, res) => {
   Post.create(req.body)
     .then(() => {
