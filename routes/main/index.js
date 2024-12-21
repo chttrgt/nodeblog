c38b51f0d1a87b0c28b5bcb941bf57c0bbd1db1d
cihatturgut <cihatturgut@yaani.com>
2024-12-21 05:39:22 +0300

    Category adding page was created and middleware was created for admin control.

--- routes/main/index.js
+++ routes/main/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
 const Post = require("../../models/Post");
-const User = require("../../models/User");
+const isAdminMiddleware = require("../../middlewares/isAdminMiddleware");
+const Category = require("../../models/Category");
 
 router.get("/", (req, res) => {
   res.render("pages/index");
@@ -16,24 +16,44 @@ router.get("/contact", (req, res) => {
   res.render("pages/contact");
 });
 
-router.get("/admin", async (req, res) => {
+//#region Kullanıcı admin ise admin sayfasına yönlendir
+router.get("/admin", isAdminMiddleware, async (req, res) => {
   try {
-    const user = await User.findById(req.session.userId);
-
-    if (!user) {
-      return res.redirect("/auth/login");
-    }
-
-    if (user.role !== "admin") {
-      return res.status(403).send("Access denied.");
-    }
-
     res.render("pages/admin");
   } catch (error) {
     console.error("Error fetching user:", error);
     res.status(500).send("An error occurred.");
   }
 });
+//#endregion
+
+//#region Kullanıcı admin ise kategori ekleme sayfasına yönlendir
+router.get("/admin/categories", isAdminMiddleware, async (req, res) => {
+  try {
+    const categories = await Category.find({}).lean();
+    res.render("pages/category", { categories });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).send("An error occurred.");
+  }
+});
+//#endregion
+
+//#region Kullanıcı admin ise yeni kategori ekleme işlemini yap
+router.post("/admin/categories", isAdminMiddleware, async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    if (!category) {
+      return res.status(400).send("Category could not be created.");
+    }
+
+    res.redirect("/admin/categories");
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).send("An error occurred.");
+  }
+});
+//#endregion
 
 router.get("/blog", (req, res) => {
   Post.find({})
